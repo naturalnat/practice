4b3b7c4ad773a0c79ac50aba518c37e54f4ccc25
Natalie Taktachev <ntaktach@gmail.com>
2021-12-26 16:14:18 -0500

    rev string - solution 2

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 //challenge 1: reverse string 
 //create function that reverses string 
 
+//sol 1
 function reverse(string){
   return string
   .split('')
   .reverse()
   .join('');
 }
+
+//sol 2 
+function reverse(string){
+    let revString = '';
+     
+    for (let i = string.length - 1; i >= 0; i--){
+        revString = revString + string[i];
+    }
+    return revString
+  }
+
