ab6a25fb01c157ad0a90d5e3757269fbd79f11be
Natalie Taktachev <ntaktach@gmail.com>
2021-12-26 16:15:49 -0500

    rev string - sol 3

--- index.js
+++ index.js
@@ -19,3 +19,12 @@ function reverse(string){
     return revString
   }
 
+//sol 3 
+function reverse(string){
+    let revString = '';
+     
+    for (let i = 0; i <= string.length -1; i++){
+        revString = string[i] + revString; 
+    }
+    return revString
+  }
