975ff71c5538074853cd0e677188ccd70f621969
Natalie Taktachev <ntaktach@gmail.com>
2022-01-02 18:09:14 -0500

    clean up - use arrow function

--- longestword.js
+++ longestword.js
@@ -8,16 +8,9 @@ function longestWord(sentence) {
     const words = sentence.match(/[a-z0-9]+/g) //global so it doesn't stop at first 
  
     //step 2 - sort by length 
-    const sorted = words.sort(function(one, two){
-        return two.length - one.length
-        //compares based on length in each iteration 
-        //returns sorted order 
-    })
+    const sorted = words.sort((one, two) => two.length - one.length)
     //step 3 - address mulitple words by putting into array 
-    const longestArray = sorted.filter(function(word){
-        //return true if word matches length of first word (bc first word is longest)
-        return word.length === sorted[0].length;
-    })
+    const longestArray = sorted.filter((word) => word.length === sorted[0].length);
     //step 4 - check array length 
     if(longestArray.length === 1){
         return longestArray[0]
