fb413aa6f8cc0083cf667ee91d7423bea3c5ab99
Natalie Taktachev <ntaktach@gmail.com>
2022-01-02 17:57:52 -0500

    longest word - step 1

---
+++ longestword.js
@@ -0,0 +1,11 @@
+//return string of longest word
+//if more than one, put into array 
+
+
+function longestWord(sentence) {
+    //remove punctuation 
+    //use match because it will return filtered array
+    const words = sentence.match(/[a-z0-9]+/g) //global so it doesn't stop at first 
+ 
+    })
+}
