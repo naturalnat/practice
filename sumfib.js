498326ed437c4d30e7dfd6ee382b529294ee42fe
Natalie Taktachev <ntaktach@gmail.com>
2022-01-16 11:34:19 -0500

    sumprime

--- sumfib.js
+++ sumfib.js
@@ -1,7 +1,18 @@
 //given a positive integer, return sum of all odd Fibonacci numbers that are less or equal to the integer 
 
 function fibsum(num){
-    return num 
+    let sum = 0; 
+    //need to calculate which number to add from
+    //keep track of fibonacci sequence 
+    let a =1; 
+    let b = 1; 
+    
+    while (b <= num){
+        if (b % 2 != 0) {
+            sum += b;
+        }
+    }
+    return sum; 
 }
 
 fibsum(4); 
---
+++ sumofprime.js
@@ -0,0 +1,7 @@
+//return sum of all prime numbers that are less than or equal to given number
+
+function sumprime(num){
+    return num 
+}
+
+sumprime(10); 
