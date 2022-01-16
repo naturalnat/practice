cbc915f6cb7fb8485f7d24418f6944de82154ea2
Natalie Taktachev <ntaktach@gmail.com>
2022-01-16 11:35:17 -0500

    if statement for 2

--- sumofprime.js
+++ sumofprime.js
@@ -1,7 +1,11 @@
 //return sum of all prime numbers that are less than or equal to given number
 
 function sumprime(num){
-    return num 
+    if (num < 2){
+        return 0; 
+    }
+
+    return num;
 }
 
 sumprime(10); 
