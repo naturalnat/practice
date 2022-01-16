e60b3275ea1c002b1febb94af4f59521d515f9fa
Natalie Taktachev <ntaktach@gmail.com>
2022-01-16 11:50:14 -0500

    sum numbers

--- sumofprime.js
+++ sumofprime.js
@@ -1,25 +1,28 @@
 //return sum of all prime numbers that are less than or equal to given number
 
-function sumprime(num){
-    if (num < 2){
-        return 0; 
+function sumprime(num) {
+    if (num < 2) {
+        return 0;
     }
     //if greater than 2: keep track of primes up to and including the paramter
     const primes = [];
-    for (let i = 2; i <= num; i++){
-        let composite = false; 
-        for (const p of primes){
-            if (i % p === 0){
-                composite = true; 
-                break; 
+    for (let i = 2; i <= num; i++) {
+        let composite = false;
+        for (const p of primes) {
+            if (i % p === 0) {
+                composite = true;
+                break;
             }
         }
-        if (!composite){
-           primes.push(i)
+        if (!composite) {
+            primes.push(i)
         }
     }
-    console.log(primes)
-    return num; 
+    let sum = 0;
+    for (const p of primes) {
+        sum += p;
+    }
+    return num;
 }
 
-sumprime(10); 
+sumprime(10);
