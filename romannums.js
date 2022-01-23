d7693decf0c1c04554f0ee2eacff0117de1a50d7
Natalie Taktachev <ntaktach@gmail.com>
2022-01-23 15:17:52 -0500

    dictionary

--- romannums.js
+++ romannums.js
@@ -1,5 +1,23 @@
 //convert number into Roman numeral 
 
+
+//remember nuances like 4 is IV!
+
+const dict = {
+    1: 'I',
+    4: 'IV',
+    5: 'V',
+    9: 'IX',
+    10: 'X',
+    40: 'XL',
+    50: 'L',
+    90: 'XL',
+    100: 'C',
+    400: 'CD', 
+    500: 'D',
+    900: 'CM',
+    1000: 'M',
+}
 function toRoman(num) {
 
 }
