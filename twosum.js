7265048e4081dadad512563803551ccb5728c3f1
Natalie Taktachev <ntaktach@gmail.com>
2022-01-30 20:09:00 -0500

    step 2 for loop

--- twosum.js
+++ twosum.js
@@ -1,12 +1,13 @@
 const array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-const sum = 15; 
+const sum = 20; 
 
 const twoSum = (array, sum) => {
-    const past = []
+    const past = [];
+    let count = 0; 
     
     for(let i = 0; i < array.length; i++){
         let curr = array[i]
-        let num = sum - array[i]
+        let needed = sum - array[i]
     }
 };
 
