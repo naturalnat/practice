dae0dad520c708e621e86cc8cbf4b6088b34903d
Natalie Taktachev <ntaktach@gmail.com>
2022-01-30 20:15:16 -0500

    class method

--- twosum.js
+++ twosum.js
@@ -1,22 +1,28 @@
+class Sums{ 
+    constructor(){}
+
+    twoSum = (array, sum) => {
+        const past = [];
+        let count = 0;
+    
+        for (let i = 0; i < array.length; i++) {
+            let curr = array[i]
+            let needed = sum - array[i]
+            if (!past.includes(needed)) {
+                past.push(curr)
+            } else {
+                return [needed, curr];
+            }
+        count++; 
+        }
+        console.log(count)
+        return "Not found!";
+    };
+    
+}
+
 const array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 const sum = 20;
 
-const twoSum = (array, sum) => {
-    const past = [];
-    let count = 0;
-
-    for (let i = 0; i < array.length; i++) {
-        let curr = array[i]
-        let needed = sum - array[i]
-        if (!past.includes(needed)) {
-            past.push(curr)
-        } else {
-            return [needed, curr];
-        }
-    count++; 
-    }
-    console.log(count)
-    return "Not found!";
-};
 
 console.log(twoSum(array, sum));
