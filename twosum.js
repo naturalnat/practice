298e23b1d5d63d9977811c79114d02cc8d333a1d
Natalie Taktachev <ntaktach@gmail.com>
2022-01-30 20:09:52 -0500

    step 3  if statement

--- twosum.js
+++ twosum.js
@@ -1,14 +1,21 @@
 const array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-const sum = 20; 
+const sum = 20;
 
 const twoSum = (array, sum) => {
     const past = [];
-    let count = 0; 
-    
-    for(let i = 0; i < array.length; i++){
+
+
+    for (let i = 0; i < array.length; i++) {
         let curr = array[i]
         let needed = sum - array[i]
+        if (!past.includes(needed)) {
+            past.push(curr)
+        } else {
+            return [needed, curr];
+        }
+    
     }
+    return "Not found!";
 };
 
 console.log(twoSum(array, sum));
