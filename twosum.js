42f253f35b5682b9ff529d9c812238c090176cf4
Natalie Taktachev <ntaktach@gmail.com>
2022-01-30 19:53:41 -0500

    2 - if statement

--- twosum.js
+++ twosum.js
@@ -2,7 +2,12 @@ const array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 const sum = 15; 
 
 const twoSum = (array, sum) => {
-
+    const past = []
+    
+    for(let i = 0; i < array.length; i++){
+        let curr = array[i]
+        let num = sum - array[i]
+    }
 };
 
 console.log(twoSum(array, sum));
